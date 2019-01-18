3b402692fcff8edee14619e14495bc2428a68ade
HaiTung <vphaitung@gmail.com>
2019-01-18 11:16:47 +0700

    +Add Update

--- SpaceShooter/Classes/AppDelegate.cpp
+++ SpaceShooter/Classes/AppDelegate.cpp
@@ -24,8 +24,6 @@
 
 #include "AppDelegate.h"
 #include "HelloWorldScene.h"
-#include "PlayGame.h"
-
 
 // #define USE_AUDIO_ENGINE 1
 // #define USE_SIMPLE_AUDIO_ENGINE 1
@@ -44,10 +42,10 @@ using namespace CocosDenshion;
 
 USING_NS_CC;
 
-static cocos2d::Size designResolutionSize = cocos2d::Size(400, 600);
-static cocos2d::Size smallResolutionSize = cocos2d::Size(400, 600);
-static cocos2d::Size mediumResolutionSize = cocos2d::Size(400, 600);
-static cocos2d::Size largeResolutionSize = cocos2d::Size(400, 600);
+static cocos2d::Size designResolutionSize = cocos2d::Size(480, 320);
+static cocos2d::Size smallResolutionSize = cocos2d::Size(480, 320);
+static cocos2d::Size mediumResolutionSize = cocos2d::Size(1024, 768);
+static cocos2d::Size largeResolutionSize = cocos2d::Size(2048, 1536);
 
 AppDelegate::AppDelegate()
 {
@@ -93,7 +91,7 @@ bool AppDelegate::applicationDidFinishLaunching() {
     }
 
     // turn on display FPS
-    director->setDisplayStats(false);
+    director->setDisplayStats(true);
 
     // set FPS. the default value is 1.0/60 if you don't call this
     director->setAnimationInterval(1.0f / 60);
@@ -120,7 +118,7 @@ bool AppDelegate::applicationDidFinishLaunching() {
     register_all_packages();
 
     // create a scene. it's an autorelease object
-    auto scene = PlayGame::createScene();
+    auto scene = HelloWorld::createScene();
 
     // run
     director->runWithScene(scene);
